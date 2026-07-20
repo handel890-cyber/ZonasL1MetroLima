077634051811cd64167aac280fd88e0d9b9641fe
handel890-cyber <handel890@gmail.com>
2026-07-20 12:13:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("🚊 Sistema SCADA - Monitoreo y Resaltado de Zonas")
+st.title("🚊 Monitoreo de Zonas")
 
 # --- CARGA AUTOMÁTICA DEL ARCHIVO JSON DESDE GITHUB ---
 JSON_LOCAL = "zonas.json"
